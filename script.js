836a0dc638c2f7e786e5ba150df0e22844774982
Sam Nick <omprakash121samuni@gmail.com>
2022-05-14 21:41:12 +0530

    commit

---
+++ script.js
@@ -0,0 +1,5 @@
+// var urlvar = 'http://127.0.0.1:5500/index.html?ar=12';
+// var url = new URL(urlvar);
+// var params = new URLSearchParams(url.search);
+// var ar = params.get('ar');
+// console.log(ar);
