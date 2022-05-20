354dfe6b4c375cbea21f84892a19891e911aa516
Sam Nick <omprakash121samuni@gmail.com>
2022-05-20 17:46:57 +0530

    commit

---
+++ atm.cpp
@@ -0,0 +1,40 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    char g, r, y, f, ff, i, n, d, o, r2;
+    cout << "Cheer:gryffindor";
+    cout << "Give me a g,";
+    cin >> g;
+    cout << "Give me a r,";
+    cin >> r;
+    cout << "Give me a y,";
+    cin >> y;
+    cout << "Give me a f,";
+    cin >> f;
+    cout << "Give me a f,";
+    cin >> ff;
+    cout << "Give me a i,";
+    cin >> i;
+    cout << "Give me a n,";
+    cin >> n;
+    cout << "Give me a d,";
+    cin >> d;
+    cout << "Give me a o,";
+    cin >> o;
+    cout << "Give me a r,";
+    cin >> r2;
+    cout << "What does it spell?";
+    cout << g << r << y << f << ff << i << n << d << o << r2;
+    return 0;
+}
--- script.js
+++ script.js
@@ -3,3 +3,22 @@
 // var params = new URLSearchParams(url.search);
 // var ar = params.get('ar');
 // console.log(ar);
+
+/*Model.............. */
+var modal = document.getElementById('cartModel');
+var btn = document.querySelector('.cartImage');
+var closeBtn = document.querySelector('.closeModelCart');
+btn.onclick = function () {
+  modal.style.display = 'block';
+  document.body.classList.add('stop-scrolling');
+};
+closeBtn.onclick = function () {
+  modal.style.display = 'none';
+  document.body.classList.remove('stop-scrolling');
+};
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = 'none';
+    document.body.classList.remove('stop-scrolling');
+  }
+};
